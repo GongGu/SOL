2cacbf1d3656b1cda1c8e30327ba7ab05cc25d1e
GongGu <37791550+GongGu@users.noreply.github.com>
2018-05-05 16:57:19 +0900

    나는 바보다

---
+++ Assets/Script/testtt.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testtt : MonoBehaviour {
+
+    //나는 바보다
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
